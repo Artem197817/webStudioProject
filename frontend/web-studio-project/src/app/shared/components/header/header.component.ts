8f0f5562c5dfb1c49ce6e80ae42c776fb0a9d259
Artem <katod1978@yandex.ru>
2025-04-23 22:05:34 +0700

    add header layout

--- frontend/web-studio-project/src/app/shared/components/header/header.component.ts
+++ frontend/web-studio-project/src/app/shared/components/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  protected headerMenuList = [
+    {title: 'Услуги', link: '/', fragment: 'services'},
+    {title: 'О нас', link: '/about', fragment: 'about'},
+    {title: 'Статьи', link: '/article', fragment: ''},
+    {title: 'Отзывы', link: '/', fragment: 'reviews'},
+    {title: 'Контакты', link: '/', fragment: 'contact'},
+  ]
+  protected isLogged: boolean = false;
 }
