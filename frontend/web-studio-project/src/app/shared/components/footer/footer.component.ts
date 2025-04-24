a2438004aa5ae65ebd6f11ea310115ef2187805c
Artem <katod1978@yandex.ru>
2025-04-24 17:21:09 +0700

    add main slider

--- frontend/web-studio-project/src/app/shared/components/footer/footer.component.ts
+++ frontend/web-studio-project/src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  protected footerMenuList = [
+    {title: 'Услуги', link: '/', fragment: 'services'},
+    {title: 'О нас', link: '/about', fragment: 'about'},
+    {title: 'Статьи', link: '/article', fragment: ''},
+    {title: 'Отзывы', link: '/', fragment: 'reviews'},
+    {title: 'Контакты', link: '/', fragment: 'contact'},
+  ]
+  protected isCall: boolean = false;
 }
--- frontend/web-studio-project/src/app/shared/components/main/main.component.ts
+++ frontend/web-studio-project/src/app/shared/components/main/main.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [
+
+  ],
   standalone: true,
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], 
 })
 export class MainComponent {
 
