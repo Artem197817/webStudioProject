d269361ee34744fdd64004ecdee3da73dee0153e
Artem <katod1978@yandex.ru>
2025-04-25 17:22:00 +0700

    add main layout

---
+++ frontend/web-studio-project/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    api: 'http://localhost:3007/api/',
+    serverStaticPath: 'http://localhost:3007/images/products/'
+  };
+  
---
+++ frontend/web-studio-project/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    api: 'http://localhost:3005/api/',
+    serverStaticPath: 'http://localhost:3005/images/products/'
+  };
+  
