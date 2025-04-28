8eb85592eb8f769501c96f57b8999038c611259f
Artem <katod1978@yandex.ru>
2025-04-28 21:54:48 +0700

    add blog begin

--- frontend/web-studio-project/src/app/shared/components/blog/blog.component.ts
+++ frontend/web-studio-project/src/app/shared/components/blog/blog.component.ts
@@ -1,10 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {ArticleService} from '../../services/article.service';
 import {ArticlesType, ArticleType} from '../../../types/article.types';
+import { CategoryType} from '../../../types/category.types';
+import {CategoryService} from '../../services/category.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   standalone: true,
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
@@ -13,17 +18,41 @@ export class BlogComponent implements OnInit{
 
   protected articles: ArticleType[] = [];
   count: number = 0;
-  pages: number = 0;
+  page: number = 1;
+  pages: number[] = [];
+  protected categories: CategoryType[];
 
-  constructor(private articleService: ArticleService) {}
+  protected isSorting: boolean = false;
+  constructor(private articleService: ArticleService,
+              private categoryService: CategoryService,
+              ) {}
 
   ngOnInit(): void {
     this.articleService.getArticles()
-    .subscribe((data: ArticlesType)=>{
-      this.articles = data.items;
-      this.count = data.count;
-      this.pages = data.pages;
-    })
+      .subscribe((data: ArticlesType) => {
+        this.articles = data.items;
+        this.count = data.count;
+        for (let index = 0; index < data.pages; index++) {
+          this.pages.push(index + 1)
+        }
+      });
+
+    this.categoryService.getCategories().subscribe({
+      next: (data) => {
+        this.categories = data;
+        this.categories.forEach(category => category.isChange = false);
+      },
+      error: (err) => {
+        console.error('Failed to load categories', err);
+      }
+    });
   }
 
+  changingIsSorting() {
+    this.isSorting = !this.isSorting;
+  }
+
+  sort(url: string) {
+
+  }
 }
